7bc0a40d3ce42c78fa08aef8468dad90a684380d
John Million <jpmillion@comcast.net>
2020-11-10 15:24:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|string| 
+  array.find {|string| cheese_types.include(string)}
 end
