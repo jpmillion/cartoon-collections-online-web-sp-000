9843604364b3a842021084a6104a659529108b10
John Million <jpmillion@comcast.net>
2020-11-10 15:22:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,16 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  return array.each_with_index {|name, index| puts "#{index + 1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  return array.map {|value| value.capitalize.insert(-1, "!")}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  return array.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
